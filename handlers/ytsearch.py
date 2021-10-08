27c8eddcbe2042c68df26d2bf896a435f987beea
τєαм ϰмαяτγ <85209355+S780821@users.noreply.github.com>
2021-10-08 06:14:15 +0530

    Update ytsearch.py

--- handlers/ytsearch.py
+++ handlers/ytsearch.py
@@ -20,10 +20,10 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 async def ytsearch(_, message: Message):
     try:
         if len(message.command) < 2:
-            await message.reply_text("/search needs an argument!")
+            await message.reply_text("/search иєє∂ѕ αи αяgυмєиτ!")
             return
         query = message.text.split(None, 1)[1]
-        m = await message.reply_text("searching....")
+        m = await message.reply_text("ѕєαяϲнιиg....")
         results = YoutubeSearch(query, max_results=4).to_dict()
         i = 0
         text = ""
